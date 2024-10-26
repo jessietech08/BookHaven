c839914aa12015eab9e5d32bf9aacac1531ae38d
Jessie <jessietajalle08@gmail.com>
2024-10-26 02:08:05 -0700

    Created CreateBookViewModel to handle user input to be stored into database

---
+++ BookHaven/Data/Migrations/20241026090509_CreateBookViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookHaven.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateBookViewModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ BookHaven/Models/CreateBookViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookHaven.Models
+{
+    public class CreateBookViewModel
+    {
+        [Required]
+        [StringLength(13, MinimumLength = 10)]
+        public string Isbn { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 1)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 1)]
+        public string Genre { get; set; }
+
+        [Required]
+        [DataType(DataType.Upload)]
+        [FileExtensions(Extensions = "jpg,jpeg,png")]
+        public IFormFile Image { get; set; } 
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+    }
+}
