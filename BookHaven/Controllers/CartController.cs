7881214524cfd7aa385b0e8f02a5ed8e6a4e316a
Jessie <jessietajalle08@gmail.com>
2024-11-24 19:17:06 -0800

    Created initial cart controller

---
+++ BookHaven/Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookHaven.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
