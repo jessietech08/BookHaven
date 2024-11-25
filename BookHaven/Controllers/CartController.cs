61556b4dc484de41f9e602c2109874d5b8c8d1e9
Jessie <jessietajalle08@gmail.com>
2024-11-24 17:23:35 -0800

    Created initial Cart Controller

---
+++ BookHaven/Controllers/CartController.cs
@@ -0,0 +1,23 @@
+﻿using BookHaven.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookHaven.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CartController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+    
+    }
+}
