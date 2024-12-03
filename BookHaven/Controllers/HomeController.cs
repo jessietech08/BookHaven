071a3a489487fbb9145bf1ea8d761ba0bf101cdf
Jessie <jessietajalle08@gmail.com>
2024-12-02 19:27:51 -0800

    Designed format for home page need to add coloring and background image

--- BookHaven/Controllers/HomeController.cs
+++ BookHaven/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using BookHaven.Data;
 using BookHaven.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -7,13 +8,21 @@ namespace BookHaven.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
+        {
+            var books = _context.Books.Take(3).ToList();
+            return View(books);
+        }
+
+        public IActionResult About()
         {
             return View();
         }
