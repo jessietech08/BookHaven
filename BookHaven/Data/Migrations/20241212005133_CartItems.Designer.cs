e17045505d960ee44fa426b6c35b30ddd4e1c953
Jessie S-Tajalle <jessietajalle08@gmail.com>
2024-12-11 16:58:11 -0800

    Edited deleteview and detailsview

--- BookHaven/Data/Migrations/20241212005133_CartItems.Designer.cs
+++ BookHaven/Data/Migrations/20241212005133_CartItems.Designer.cs
@@ -4,6 +4,7 @@ using BookHaven.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookHaven.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241212005133_CartItems")]
+    partial class CartItems
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
